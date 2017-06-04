0f44e9942f7c71503aa0454ababc7bee09e873f1
Aaron Mady <defiant00@gmail.com>
2017-06-04 15:16:32 -0400

    latest

---
+++ Chromatic/Assets/Scripts/Factory.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class Factory
+{
+	public static GameObject Tile(string name, int x = 0, int y = 0)
+	{
+		var tile = new GameObject("t_" + x + "_" + y);
+		var sr = tile.AddComponent<SpriteRenderer>();
+		sr.sprite = Resources.Load<Sprite>("Sprites\\Tiles\\" + name);
+		sr.transform.position = new Vector3(x, y);
+		return tile;
+	}
+}
---
+++ Chromatic/Assets/Scripts/LevelEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class LevelEditor : MonoBehaviour
+{
+	GameObject tile;
+
+	void Start()
+	{
+		Factory.Tile("gwgw_g", 0, 0);
+		tile = Factory.Tile("gwgw_w", 1, 1);
+	}
+
+	void Update()
+	{
+		Destroy(tile, 2);
+	}
+}
